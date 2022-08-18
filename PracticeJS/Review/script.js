18828eded1b6832ea823844fb83f2295f867dbf4
Drapetes Dandridge <drapetesdandridge@cats-mbp.lan>
2022-08-18 14:51:42 -0400

    js review lab

---
+++ PracticeJS/Review/script.js
@@ -0,0 +1,30 @@
+/*
+1. How do we assign a value to a variable
+A: with an assignment operator =
+    variable = value
+    e.g. let numX = 3;
+
+2.  How do we change the value of a variable 
+A:  with a new assignment statment, 
+    reassigning a new value to the variable with =
+
+3.  How assign an existing variable to another variable
+A:  again with an assignment operator
+e.g.    let x = 0 
+        let y = x
+
+4.  declare: to say that an element exists
+    assign: to give that element a value 
+    define: a variable is defined if its been declared and assigned a value 
+            a function is defined using the function key word, a name, parameters, and 
+            a body of code to execute.
+
+5.  pseudo coding: to explain in plain english the steps, 
+    logic, or algorithm you will use to solve a problem 
+    or create a program. We do this to break our coding 
+    process into digestible steps and organize our thought 
+    it gives reason to each line of code, making it more efficient, 
+    trackable, readable etc.
+
+6.  More time should be spent thinking/planning. less time coding. is it 50/50 or 60/40     
+    */
