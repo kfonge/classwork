ed730cda47aec0d2ff356fe7fce2cfc5730cfd9a
Kolenge <kfonge@gmail.com>
2022-09-07 10:24:58 -0400

    modals

---
+++ Modals/script.js
@@ -0,0 +1,12 @@
+// alert("Hello! I am an alert box!!");
+
+
+
+//modal has a header and paragraph and background is greyed out
+
+//inside the modal....
+const btn1 = document.querySelector("button")
+btn1.addEventListener("click", (evt) => {
+    
+})
+const mainDiv = document.getElementById("btnContainer")
