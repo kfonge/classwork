2a03788b5eabc4bb35dc3d5b2ebb714384fa1aec
Kolenge <kfonge@gmail.com>
2022-09-28 18:57:01 -0400

    office hours server from scratch

--- Node_Practice/Office_Hours_Comments/Steps.md
+++ Node_Practice/Office_Hours_Comments/Steps.md
@@ -1,9 +1,9 @@
 Server file:
 1. Load express
-2.
-3.
-4.
-5.
+2. Install packages
+3. Creates express app (object)
+4. Identify AND Listen on port (double check in browser)
+5. Create new route (app.get) with req/res, (INDUCES)
 
 Route Files:
 1.
--- Node_Practice/Office_Hours_Comments/server.js
+++ Node_Practice/Office_Hours_Comments/server.js
@@ -10,8 +10,31 @@ const express = require("express")
 
 //Identify (packaged?) Routes
 
+// Creates our express app (object)
+const app = express()
+
+// Setup new Route for Request and Response
+// app.get with 2 parameters
+// 1: file path 
+// 2: callbackfunction with homies
+
+app.get('/', (req, res) => { 
+    res.send (
+        `<h1> List of Comments Received</h1> 
+        
+        <form> 
+        <label htmlFor="comment">Leave a Comment:</label>
+        <input type="text" id="comment" name="name" />
+        </form>`
+
+    )
+})
+
+
+
 
 //Identify the port
+const port = 3000;
 
-//Listen to port
-
+//Listen on port
+app.listen(port, () => console.log("Listening on port: ", port))
